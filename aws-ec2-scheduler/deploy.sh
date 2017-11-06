c05cc21c571b19c1f65220a493bd32bab2cdd212
Leonardo Silva <leogsilva@Leonardos-MacBook-Pro.local>
2017-11-06 09:48:23 -0200

    Initial version of cloudformation for postgresql rds

--- aws-ec2-scheduler/deploy.sh
+++ aws-ec2-scheduler/deploy.sh
@@ -1,1 +1,4 @@
-aws cloudformation deploy --profile ihmlabs --region us-east-1 --template-file cform/ec2-scheduler.output.template --stack-name ihm-tools-ec2-scheduler --parameter-overrides CustomTagName=scheduler:ec2-startstop Schedule=5minutes DefaultStartTime=1100 DefaultStopTime=2200 DefaultDaysActive=weekdays DynamoDBTableName=EC2-Scheduler DynamoDBTableNameState=EC2-Scheduler-State ReadCapacityUnits=1 WriteCapacityUnits=1 CloudWatchMetrics=Disabled SendAnonymousData=No --capabilities CAPABILITY_IAM
+#!/bin/bash
+
+PROFILE=$1
+aws cloudformation deploy --profile $PROFILE --region us-east-1 --template-file cform/ec2-scheduler.output.template --stack-name tools-ec2-scheduler --parameter-overrides CustomTagName=scheduler:ec2-startstop Schedule=5minutes DefaultStartTime=1100 DefaultStopTime=2200 DefaultDaysActive=weekdays DynamoDBTableName=EC2-Scheduler DynamoDBTableNameState=EC2-Scheduler-State ReadCapacityUnits=1 WriteCapacityUnits=1 CloudWatchMetrics=Disabled SendAnonymousData=No --capabilities CAPABILITY_IAM
--- aws-ec2-scheduler/package.sh
+++ aws-ec2-scheduler/package.sh
@@ -1,1 +1,5 @@
-aws cloudformation package --profile ihmlabs --region us-east-1 --s3-bucket br-com-ihm-cloudformation --template-file cform/ec2-scheduler.template --output-template-file cform/ec2-scheduler.output.template
+#!/bin/bash
+
+PROFILE=$1
+BUCKET_NAME=$2
+aws cloudformation package --profile $PROFILE --region us-east-1 --s3-bucket $BUCKET_NAME --template-file cform/ec2-scheduler.template --output-template-file cform/ec2-scheduler.output.template
